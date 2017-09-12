6e0977dad291d5964f50a334d5e505a62aa0740d
KentShepard <kentshepard7@gmail.com>
2017-09-11 20:29:53 -0400

    Link the templates to each component and populate the video list with example data

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,17 @@
 angular.module('video-player')
 .component('videoList', {
-  // TODO
+
+  controller: function() {
+    this.videos = exampleVideoData;
+
+    this.addVideoListEntry = () => {
+
+    }
+
+    this.onClick = () => {
+
+    }
+  },
+
+  templateUrl: 'src/templates/videoList.html'
 });
