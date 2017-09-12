1b4f9e71f6806df1702d7465f209dffe6c63fc3a
Kent Shepard <kentshepard7@gmail.com>
2017-09-12 19:38:30 -0400

    Implement click on video titles

--- src/components/app.js
+++ src/components/app.js
@@ -2,8 +2,14 @@ angular.module('video-player')
 .controller('AppCtrl', function() {
   this.videos = exampleVideoData;
   this.currentVideo = exampleVideoData[0];
+
+  this.selectVideo = (video) => {
+    this.currentVideo = video;
+  }
 })
 .component('app', {
   controller: 'AppCtrl',
   templateUrl: 'src/templates/app.html'
 })
+
+//
