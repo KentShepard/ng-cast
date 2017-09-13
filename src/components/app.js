6c30741e4b4564417791efa133249dfdd7b0f12d
Kent Shepard <kentshepard7@gmail.com>
2017-09-12 20:43:09 -0400

    Implement search feature

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,23 @@
 angular.module('video-player')
-.controller('AppCtrl', function() {
+.controller('AppCtrl', function(youTube) {
   this.videos = exampleVideoData;
   this.currentVideo = exampleVideoData[0];
 
   this.selectVideo = (video) => {
     this.currentVideo = video;
   }
+
+  this.searchService = youTube;
+
+  this.searchResults = (response) => {
+    this.videos = response;
+    this.currentVideo = this.videos[0];
+  }
+
+  youTube.search('kittens', this.searchResults);
+
 })
 .component('app', {
   controller: 'AppCtrl',
   templateUrl: 'src/templates/app.html'
 })
-
-//
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,25 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function($http, $window){
+  this.search = function(query, callback) {
+    $http.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        q: query,
+        maxResults: 5,
+        type: 'video',
+        part: 'snippet',
+        videoEmbeddable: 'true',
+        key: $window.YOUTUBE_API_KEY
+      }
+    })
+    .then(function({data}) {
+      if (callback) {
+        callback(data.items);
+      }
+    })
+    .catch(function({data}) {
+      data.error.errors.forEach(function(err) {
+        console.error(err.message);
+      });
+    });
+  };
 });
